71b706ad4b3c8b65d186833e6e08fb489a547947
subhradip32 <83198378+subhradip32@users.noreply.github.com>
2021-04-30 09:01:15 +0530

    Add files via upload

---
+++ pythonProject1/aasdfadad.py
@@ -0,0 +1,57 @@
+first = 'subhradip'
+last = 'majumder'
+jam= 'subiir majumder'
+secind= 'dipa majumder'
+
+print("who is the father of subhradip majumder ?")
+msg = f'{jam} is the father of the {first } {last}'
+print(msg)
+
+print("who is the mother of subhradip majumder ?"
+      )
+print("hiii")
+
+x=10
+y=36
+z = 41
+
+
+print(x+y)
+print(x/y)
+print(x+y+2*x)
+
+r= z % 2
+if r==0:
+      print("oh yaa ")
+      print(("you are too much close"))
+      if z > 1:
+            print("offff")
+
+if r==1:
+      print("odd")
+      print("here is your pass word ")
+      if z > 1:
+            print("you are my jewel ")
+
+
+print("what is your age ")
+
+while 0 == 0 :
+    try:
+        age  = input()
+        age  = int(age)
+        if age > 18:
+            print("you are a adult  ")
+        break
+    except:
+        print("sona number lakho......")
+
+for letter in  'Python':     # First Example
+   print('Current Letter :', letter)
+
+
+
+
+
+
+
